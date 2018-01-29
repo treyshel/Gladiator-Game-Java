ebdb2e4cc7d298b1078d80dd5ecd468cdfb6333f
treyshel <tshelton@basecampcodingacademy.org>
2018-01-29 11:10:58 -0600

    after getting the main file finished, I can now get input from the user so the two gladiators can now attack and heal.. going to start on more battle options later

--- src/com/company/Gladiator.java
+++ src/com/company/Gladiator.java
@@ -15,7 +15,7 @@ public class Gladiator {
     Random rand = new Random();
 
     Gladiator(String name) {
-        name = name;
+        this.name = name;
         health = 150;
         rage = 0;
         /*rand.nextInt((max - min) + 1) + min;*/
@@ -23,5 +23,40 @@ public class Gladiator {
         high_damage = rand.nextInt((30 - 20) + 1) + 20;
     }
 
-    
+    public void attack(Gladiator defender) {
+        /*line 29: damage is going to be a random integer
+        choosing between the high and low damage*/
+        int random_damage = rand.nextInt(high_damage) + low_damage;
+        /*//lines 32-33: if the randomly generated damage is below the amount
+        of rage the attacker has, the damage will be doubled to the opponent*/
+        if (rand.nextInt((100 - 1) + 1) + 1 <= rage) {
+            defender.health -= (2 * random_damage);
+            rage = 0;
+            System.out.println("CRITCAL HIT OF " + random_damage * 2 + "\n");
+        } else {
+            /*lines 38-39: if the random number isn't below the attacker's rage,
+            the damage will just be the randomly generated number and rage will go up*/
+            defender.health -= random_damage;
+            rage += 15;
+            System.out.println("HIT OF " + random_damage + "\n");
+            //this keeps from the losing opponent's health from going below 0
+            defender.health = Math.max(0, defender.health);
+        }
+    }
+
+    public void heal() {
+        health += 15;
+    }
+
+    public boolean isDead() {
+        if (health == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String name() {
+        return this.name;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,65 @@
 package com.company;
 
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.Scanner;
+
 public class Main {
 
+    public static String get_choice(Gladiator glad_name) {
+        String attacker_choice;
+        Scanner choice_input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Gladiator" + " " + glad_name.name + " " + "|HP" + glad_name.health + "| |RAGE" + glad_name.rage + "| |LowDamage" + glad_name.low_damage + "| |HighDamage" + glad_name.high_damage + "|\n[A]ttack\n[H]eal");
+            attacker_choice = choice_input.next().toUpperCase();
+            if (attacker_choice.equals("A")) {
+                return "A";
+            } else if (attacker_choice.equals("H")) {
+                return "H";
+            } else {
+                System.out.println("INVALID CHOICE");
+            }
+        }
+    }
+
+    public static void battle(Gladiator attacker, Gladiator defender, String choice) {
+        if (choice.equals("A")) {
+            attacker.attack(defender);
+        } else if (choice.equals("H")) {
+            attacker.heal();
+        }
+    }
+
     public static void main(String[] args) {
+        String Glad1_name;
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Gladiator 1 -> Name: ");
+        Glad1_name = user_input.next();
+        Gladiator Glad1 = new Gladiator(Glad1_name);
+
+        String Glad2_name;
+        Scanner user_input2 = new Scanner(System.in);
+        System.out.println("Gladiator 2 -> Name: ");
+        Glad2_name = user_input2.next();
+        Gladiator Glad2 = new Gladiator(Glad2_name);
+
+        while (true) {
+            if (!Glad1.isDead()) {
+                String choice = get_choice(Glad1);
+                battle(Glad1, Glad2, choice);
+                if (Glad2.isDead()) {
+                    System.out.println("Gladiator" + Glad1_name + "WINS!!");
+                    break;
+                }
+            }
+            if (!Glad2.isDead()) {
+                String choice = get_choice(Glad2);
+                battle(Glad2, Glad1, choice);
+                if (Glad1.isDead()) {
+                    System.out.println("Gladiator" + Glad2_name + "WINS!!");
+                    break;
+                }
+            }
+        }
     }
 }
